ea7a86ae7b99afb5ab01b74ef74ea73d40b829ed
Hadi Nur <hadi.wahid@smartfren.com>
2021-10-17 09:29:47 +0700

    wip: home page and its header component

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -3,17 +3,33 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyles = createGlobalStyle`
 	@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500&display=swap');
 
+	html {
+		--orange: #FF3527;
+		--black: #393939;
+		--grey: #C6C6C6;
+		--lightOrange: #ff5f53;
+		--lightGrey: #969696;
+		--offWhite: #ededed;
+		box-sizing: border-box;
+		font-size: 62.5%;
+	}
+
 	*, *::before, *::after {
 		margin: 0;
 		padding: 0;
+		box-sizing: inherit;
 	}
-	html {
-		font-size: 62.5%;
-	}
+
 	body {
 		font-family: 'Roboto', sans-serif;
 		font-size: 1.6rem;
+		color: var(--black);
 	}
+
+	a {
+		text-decoration: none;
+		color: var(--black);
+  	}
 `
 
 export default GlobalStyles
--- src/components/home-header/HomeHeaderStyles.js
+++ src/components/home-header/HomeHeaderStyles.js
@@ -6,4 +6,41 @@ export const HomeHeaderStyles = styled.div`
 	align-items: center;
 	padding: 2rem;
 	background-color: #fff;
+
+	.left-header {
+		.title {
+			color: var(--orange);
+			font-size: 2.5rem;
+			font-weight: 800;
+		}
+		.subtitle {
+			color: var(--grey);
+			margin-top: .5rem;
+		}
+	}
+	.right-header {
+		.search {
+			padding:  1rem .5rem;
+			position: relative;
+			border: 1px solid var(--grey);
+			border-radius: .5rem;
+			outline: none;
+			color: var(--lightGrey);
+			font-size: 1.5rem;
+			margin-right: 1rem;
+		}
+		.add-button {
+			padding:  1rem .5rem;
+			border: 1px solid var(--orange);
+			border-radius: .5rem;
+			font-size: 1.5rem;
+			background-color: var(--orange);
+			color: #fff;
+			text-transform: capitalize;
+			cursor: pointer;
+			&:hover {
+				background-color: var(--lightOrange);
+			}
+		}
+	}
 `
