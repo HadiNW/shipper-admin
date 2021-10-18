fd405510e0314d1147eed59df798e1572725894e
Hadi Nur <hadi.wahid@smartfren.com>
2021-10-18 17:56:54 +0700

    add lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
 
+import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Layout from './components/layout/Layout'
+import Spinner from './components/spinner/Spinner'
 import GlobalStyles from './GlobalStyles'
-import Dashboard from './pages/Dashboard'
-import HomePage from './pages/HomePage'
-import Pickup from './pages/Pickup'
+
+const HomePage = React.lazy(() => import('./pages/HomePage'))
+const Pickup = React.lazy(() => import('./pages/Pickup'))
+const Dashboard = React.lazy(() => import('./pages/Dashboard'))
+
 function App() {
 	return (
 		<>
 			<GlobalStyles />
 			<Layout>
 				<Switch>
-					<Route path='/' exact>
-						<HomePage />
-					</Route>
-					<Route path='/dashboard' exact>
-						<Dashboard />
-					</Route>
-					<Route path='/pickup' exact>
-						<Pickup />
-					</Route>
+					<React.Suspense fallback={<Spinner />}>
+						<Route path='/' exact>
+							<HomePage />
+						</Route>
+						<Route path='/dashboard' exact>
+							<Dashboard />
+						</Route>
+						<Route path='/pickup' exact>
+							<Pickup />
+						</Route>
+					</React.Suspense>
 				</Switch>
 			</Layout>
 		</>
